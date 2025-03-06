9e52aee39f992c4d6eec737d4b29fa9d66992dbf
mdmonis <mdmonis128@gmail.com>
2025-03-06 17:53:34 +0530

    Server side rendering started

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require("express");
+const path = require("path");
 const { connectToMongoDB } = require("./connect");
 const urlRoute = require("./routes/url");
 const URL = require("./models/url");
 const app = express();
 const PORT = 8001;
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 app.use(express.json());
 connectToMongoDB("mongodb://127.0.0.1:27017/short-url").then(() =>
   console.log("Connected")
 );
 app.use("/url", urlRoute);
+app.get("/test", (req, res) => {
+  res.end("<h1>Md Monis sssss</h1>");
+});
 app.get("/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
   const entry = await URL.findOneAndUpdate(
